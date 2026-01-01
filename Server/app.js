4832a6d7845fb77f52450a6970ef9e5906861e0f
Redowan0019 <146367158+Redowan0019@users.noreply.github.com>
2026-01-02 04:07:06 +0600

    refactor: reorganize imports, update CORS configuration, and add database connection

--- Server/app.js
+++ Server/app.js
@@ -1,7 +1,7 @@
-import express from "express";
-import { config } from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import { config } from "dotenv";
+import express from "express";
 import "express-fileupload";
 import fileUpload from "express-fileupload";
 
@@ -11,21 +11,19 @@ config({ path: "./config/config.env" });
 
 app.use(
     cors({
-    origin: "http://localhost:3000",
+    origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
     })
 );
 
-app.use(
-    fileUpload({
-    useTempFiles: true,
-    tempFileDir: "./temp",
-    })
-);
-
-app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(fileUpload({
+    useTempFiles: true,
+    tempFileDir: "./uploads",
+}))
 
 export default app;
