86c976b2e3cfe9ead8af70677a4ed6f6a76e5c20
Redowan0019 <146367158+Redowan0019@users.noreply.github.com>
2026-01-03 05:47:01 +0600

    feat: add password reset functionality, integrate nodemailer for email notifications, and update SMTP configuration

--- Server/router/authRoutes.js
+++ Server/router/authRoutes.js
@@ -1,9 +1,13 @@
 import express from "express";
 import {
+    forgotPassword,
     getUser,
     login,
     logout,
-    register
+    register,
+    resetPassword,
+    updatePassword,
+    updateProfile,
 } from "../controllers/authController.js";
 import { isAuthenticated } from "../middlewares/authMiddleware.js";
 
@@ -11,11 +15,11 @@ const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
-router.get("/me", isAuthenticated, getUser);
+router.get("/getUser", isAuthenticated, getUser);
 router.get("/logout", isAuthenticated, logout);
-// router.post("/password/forgot", forgotPassword);
-// router.put("/password/reset/:token", resetPassword);
-// router.put("/password/update", isAuthenticated, updatePassword);
-// router.put("/profile/update", isAuthenticated, updateProfile);
+router.post("/password/forgot", forgotPassword);
+router.put("/password/reset/:token", resetPassword);
+router.put("/password/update", isAuthenticated, updatePassword);
+router.put("/profile/update", isAuthenticated, updateProfile);
 
 export default router;
